00322af0ce76cac447d1deb2a964334eafbc47f9
Megan Warnock <megandwarnock@gmail.com>
2017-04-04 17:06:31 -0700

    pass first test for book class

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -5,7 +5,7 @@ public class DatabaseRule extends ExternalResource {
 
   @Override
   protected void before() {
-    DB.sql2o = new Sql2o("jbdc:postgresql://localhost:5432/library_catalog_test", null, null);
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/library_catalog_test", null, null);
   }
 
   @Override
